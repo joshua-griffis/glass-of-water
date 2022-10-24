cad71db4e0e7c2323de7253105c36fc111e3517a
Joshua <joshuagiff03@email.com>
2022-10-24 10:05:45 -0500

    first commit

---
+++ glass_of_water.py
@@ -0,0 +1,13 @@
+class Glass:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.amount = 0
+
+    def pour_in(self, amount):
+        self.amount += amount
+        if self.amount >= self.capacity:
+            self.amount = self.capacity
+
+    def pour_out(self, amount):
+        self.amount -= amount
+        self.amount = max(0, self.amount)
